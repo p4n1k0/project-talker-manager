7d5c7b552a5d71786dc317b7837a996fbdb57830
Tarick <gabre.tarick@gmail.com>
2022-08-26 18:24:46 -0300

    requisito 2

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -4,9 +4,21 @@ const { getAll } = require('../db/talkerDB');
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
-    const result = await getAll();
-    
-    res.status(200).json(result);
+    const data = await getAll();
+
+    res.status(200).json(data);
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await getAll();
+    const talker = data.find((t) => t.id === Number(id));
+
+    if (talker) {
+    return res.status(200).json(talker);
+    }
+
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = router;
