fb1f117190076152eff1b20fe63617b5aaa19b69
Tarick <gabre.tarick@gmail.com>
2022-08-27 11:57:36 -0300

    3 passando no terminal

--- src/route/talkerRouter.js
+++ src/route/talkerRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { getAll } = require('../db/talkerDB');
 const generateToken = require('../utils/generateToken');
-const { validateEmail, validatePassword } = require('../middleware/validateLogin');
 
 const router = express.Router();
 
@@ -23,8 +22,10 @@ router.get('/:id', async (req, res) => {
     res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
-router.post('/', validateEmail, validatePassword, (_req, res) => {
+router.post('/', async (req, res) => {
+    const { email, password } = req.body;
     const token = generateToken();
+    console.log(email, password);
 
     res.status(200).json({ token });
   });
